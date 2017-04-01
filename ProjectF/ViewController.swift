db35b55261c143e2e40daaccb40d60ab2cda1e68
= <bernardcosgriff@gmail.com>
2017-04-01 17:49:38 -0600

    Initial Commit

---
+++ ProjectF/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProjectF
+//
+//  Created by Bernard Cosgriff on 4/1/17.
+//  Copyright © 2017 Bernard Cosgriff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
